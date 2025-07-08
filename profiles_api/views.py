639186485a773e4d27b2a3206ae7c52ebc5d5efb
latha-k <lathajava1@gmail.com>
2025-07-07 20:11:01 -0700

    Added viewset to profiles_api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,11 +2,13 @@ from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response  import Response
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
 
 class HelloApiView(APIView):
+       
     serializer_class = serializers.HelloSerializer
     
     def get(self, request, format=None):
@@ -37,3 +39,34 @@ class HelloApiView(APIView):
     def delete(self,request, pk=None):
         
         return Response({"method":'DELETE'})
+    
+    
+class HelloViewSet(viewsets.ViewSet):
+    
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        a_viewset = [
+            'uses actions (list, create, retrive, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+        ]
+        return Response({'message':'Hello!', 'a_viewset': a_viewset})
+    
+    def create(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    def retrieve(self, request, pk=None):
+        return Response({'http_method':'GET'})
+    
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+    def destroy(self, request, pk=None):
+        return Response({'http_method':'DELETE'})
